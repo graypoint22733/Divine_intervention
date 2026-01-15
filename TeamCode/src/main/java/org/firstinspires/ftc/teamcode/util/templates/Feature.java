ca31c1f22714ee25023cdd757362bcf3d5d9f8e6
A-coder152 <abdullahalsaleem19@gmail.com>
2026-01-14 20:41:17 -0500

    oopsie daisy #1 (file does not exist)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/templates/Feature.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/templates/Feature.java
@@ -1,15 +1,15 @@
 package org.firstinspires.ftc.teamcode.util.templates;
 
-import org.firstinspires.ftc.teamcode.util.wrappers.Hardware;
+//import org.firstinspires.ftc.teamcode.util.wrappers.Hardware;
 
 import java.util.ArrayList;
 
 public abstract class Feature {
-    public final ArrayList<Hardware> hw;
+    // public final ArrayList<Hardware> hw;
 
-    protected Feature(ArrayList<Hardware> hw) {
-        this.hw = hw;
-    }
+    // protected Feature(ArrayList<Hardware> hw) {
+    //     this.hw = hw;
+    // }
 
     public abstract void update();
 }
