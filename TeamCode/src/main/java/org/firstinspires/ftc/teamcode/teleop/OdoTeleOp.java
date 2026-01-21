1666e5f5ee3fc579e145b88869405c075059db75
A-coder152 <abdullahalsaleem19@gmail.com>
2026-01-20 22:52:05 -0500

    oopsie daisy #6.76

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/OdoTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/OdoTeleOp.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2d;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
@@ -17,7 +17,7 @@ public class OdoTeleOp extends LinearOpMode{
 
     @Override
     public void runOpMode(){
-        odo = map.get(GoBildaPinpointDriver.class, "odo");
+        odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
         odo.setOffsets(0, 0);
         odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
         odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD,
@@ -27,7 +27,7 @@ public class OdoTeleOp extends LinearOpMode{
 
         while(opModeIsActive()){
             odo.update();
-            Pose2D pos = odo.getPosition();
+            Pose2d pos = odo.getPosition();
             String data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}",
                 pos.getX(DistanceUnit.MM),
                 pos.getY(DistanceUnit.MM),
