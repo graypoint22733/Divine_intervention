6202f33dea4308bda7272ac438aa78758ba2894b
A-coder152 <abdullahalsaleem19@gmail.com>
2026-01-22 00:45:31 -0500

    better anglewrap hopefully

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/wrappers/Sensorange.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/wrappers/Sensorange.java
@@ -24,9 +24,9 @@ public class Sensorange{
     public void calculateValue() {
         double encoderResult = AngleUnit.normalizeDegrees((encoder.getVoltage() - 0.043) / 3.1 * 360);
 
-        if (lastCheckedPosition >= 160 && encoderResult <= -160) {
+        if (lastCheckedPosition >= 100 && encoderResult <= -100) {
             fullRotations++;
-        } else if (lastCheckedPosition <= -160 && encoderResult >= 160) {
+        } else if (lastCheckedPosition <= -100 && encoderResult >= 100) {
             fullRotations--;
         }
 
