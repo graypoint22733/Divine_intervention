5736f4ee35a811e345844818ed0e6dbe62e34886
A-coder152 <abdullahalsaleem19@gmail.com>
2026-02-11 00:26:52 -0500

    tt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
@@ -33,7 +33,7 @@ public class Turret {
 
         // Error is the difference between goal and current robot heading
         // We subtract Math.PI if the turret "zero" is facing the back of the robot
-        double error = angleWrap(angleToGoal - currentPosition.getHeading() - Math.PI);
+        double error = angleWrap(angleToGoal + currentPosition.getHeading());
 
         // Map the radian error to a 0.0 - 1.0 servo value using gear ratio
         double servoDelta = (error * GEAR_RATIO) / SERVO_RANGE_RAD;
