d27eeef0d5fc4a7ca9333af85840e31adf542553
9581-0 <tmaekawa2027@ravenscroft.org>
2026-02-11 00:47:58 -0500

    hoodopodsafha;hsfgdAAAAAAAAAAA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/HoodOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/HoodOp.java
@@ -14,14 +14,14 @@ public class HoodOp extends LinearOpMode {
         // Initialize the hardware variables.
         // The string "hood" must match the name in your Robot Configuration.
         hood = hardwareMap.get(Servo.class, "hood");
-double pos = 0.5;
+double pos = 0.4;
         waitForStart();
         while (opModeIsActive()) {
             hood.setPosition(pos);
 
-            if (gamepad1.dpad_up) {pos += 0.01;}
-            if (gamepad1.dpad_down) {pos -= 0.01;}
-            telemetry.addData("Servo Position", Math.toDegrees(hood.getPosition()));
+            if (gamepad1.dpad_up) {pos += 0.001;}
+            if (gamepad1.dpad_down) {pos -= 0.001;}
+            telemetry.addData("Servo Position", 360* (hood.getPosition()));
             telemetry.addData("Status", "Running - Forced to 0");
             telemetry.update();
 
